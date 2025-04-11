3b45489a70137fda70bc5652ad7de9a441eec73d
MauricioChaves00 <Maucastro621@gmail.com>
2025-04-10 23:42:18 -0600

    se modifica el proyecto y se remplaza por uno nuevo

--- src/javamatriz/JavaMatriz.java
+++ src/javamatriz/JavaMatriz.java
@@ -2,42 +2,37 @@
 package javamatriz;
 
 import java.util.Scanner;
-
 public class JavaMatriz {
 
     // llevar a cabo un programa que permita cargar completamente con numeros 5 una matriz de 4 filas 5 columnas 
     public static void main(String[] args) {
-        int nFilas = 0,nColumnas = 0;
-        Scanner teclado = new Scanner (System.in);
-        int matriz [][] = new int [4][5];
+        Scanner entrada = new Scanner(System.in);
+        double matriz [][] = new double[4][4];
         
-        
-        
-        
-        // rellenar matriz con numeros 5
-        
-        
-        
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = 5;
-            }
-            System.out.println("");
-        }
-        // mostrar matriz
-       
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j]);
+        double nota,suma = 0.0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("ingrese la nota del alumno numero ["+i+"]");
+                matriz[i][j] = entrada.nextDouble();
+                suma = suma +matriz[i][j];
                 
             }
-            System.out.println("");
+            matriz[i][3] = suma/3;
+            suma =0.0;
+            
         }
-       
         
         
         
-        
+        for (int i = 0; i < 4; i++) {
+            System.out.println("la nota del alumno "+i+ "son:" );
+            for (int j = 0; j < 3; j++) {
+                
+                System.out.println("nota numero " + j + " " + matriz[i][j]);
+                
+            }
+            System.out.println("el promedio de las notas es: "+ matriz[i][3]);
+            }
         
      
     }
